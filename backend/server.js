960137f43207441cf00a4e83094aee94cc36bfdd
Sp Ramanathan <subburamanathan7@gmail.com>
2023-10-28 14:38:24 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -7,19 +7,27 @@ const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middlewares/errorMiddleware')
 const connectDB = require('./config/dbconfig');
 const cors = require('cors')
+const path = require('path')
 
 
 connectDB()
 const app = express();
+dotenv.config();
 
 app.use(express.json()) //Body Parser
 app.use(express.urlencoded({extended:false})) //urlEncoded
 app.use(cors()) //Cross-Orgin Access
 
+//Static Files
 
+app.use(express.static(path.join(__dirname,'../frontend/build')))
 
 app.use('/api/database',require('./routes/contactRoutes'));
 app.use('/api/volunteer',require('./routes/volunteerRoutes'));
+
+app.get('*',function(req,res){
+    res.sendFile(path.join(__dirname,'../frontend/build/intex.html'))
+})
 
 app.use(errorHandler);//Overides default ErrorHandler
 app.listen(port,()=> console.log(`App up and running on ${port}`))
